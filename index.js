51a41547b9e95287facb88e92cbaf1843622c655
Rashmi Karnik <rashmisag@gmail.com>
2020-06-03 20:23:07 -0700

    Node MongoDb-1

---
+++ index.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+/*mongodb is a driver and .MongoClient is a object */
+const assert = require('assert').strict;
+
+/*url where mongo server is running */
+const url = 'mongodb://localhost:27017/';
+/*set database name to use*/
+const dbname = 'nucampsite';
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+    assert.strictEqual(err, null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+
+
+    db.dropCollection('campsites', (err, result) => {
+      
+       assert.strictEqual(err, null);
+        console.log('Dropped Collection', result);
+        
+
+        const collection = db.collection('campsites');
+
+        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
+        (err, result) => {
+            assert.strictEqual(err, null);
+            console.log('Insert Document:', result.ops);
+
+            collection.find().toArray((err, docs) => {
+                assert.strictEqual(err, null);
+                console.log('Found Documents:', docs);
+
+                client.close();
+            });
+        });
+    });
+});
